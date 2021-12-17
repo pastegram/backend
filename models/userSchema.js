9429699a3279c84554873c85a1cfbe2e6c8f3cb7
Varun <lanjharav@gmail.com>
2021-12-17 22:48:45 +0530

    Fixed stuff

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,15 +4,33 @@ const userSchema = mongoose.Schema(
   {
     username: {
       type: String,
+      unique: true,
+      required: true,
+      min: 4,
+      max: 30,
     },
     email: {
       type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email address",
+      ],
     },
     password: {
       type: String,
+      required: true,
+      min: 8,
+      max: 24,
     },
     userpfp: {
       type: String,
+    },
+    bio: {
+      type: String,
+      default: "",
+      max: 200,
     },
     followers: {
       type: Array,
