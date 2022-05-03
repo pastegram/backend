cc5329c7503109a76179a150249ac7e3a09d7725
Py Bash <67195650+pybash1@users.noreply.github.com>
2022-05-03 18:04:40 +0530

    Minor changse

--- src/api.py
+++ src/api.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 import auth
 import user
 
-api = FastAPI()
+api = FastAPI(title="Pastegram", version="0.1.0", description="The next generation of pastebin!", debug=True)
 
 api.include_router(auth.router, tags=["auth"], prefix="/auth")
 api.include_router(user.router, tags=["user"])
